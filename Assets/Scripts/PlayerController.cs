0d2bac7e3fc93e7d1cd3f9a8d3a8d4c4239c29e3
Fred Sauer <fredsa@google.com>
2015-07-11 16:20:48 -0700

    smarter clamping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public Transform grid;
+
 	float speed = .15f;
+	float clampX;
+	float clampy;
+
+	void Start() {
+		clampX = grid.localScale.x/2 - transform.localScale.x/2;
+		clampy = grid.localScale.y/2 - transform.localScale.y/2;
+	}
 
 	void FixedUpdate () {
 		float h = Input.GetAxis ("Horizontal") * speed;
 		float v = Input.GetAxis ("Vertical") * speed;
 
-		float x = Mathf.Clamp (transform.localPosition.x + h, -1f, 1f);
-		float y = Mathf.Clamp (transform.localPosition.y + v, -1.5f, 1.5f);
+		float x = Mathf.Clamp (transform.localPosition.x + h, -clampX, clampX);
+		float y = Mathf.Clamp (transform.localPosition.y + v, -clampy, clampy);
+		float z = transform.localPosition.z;
 
-		gameObject.transform.localPosition = new Vector3 (x, y, 0);
+		gameObject.transform.localPosition = new Vector3 (x, y, z);
 	}
 }
