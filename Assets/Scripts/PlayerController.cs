38a5a1fc8c9c2c09dcb49ea85cc9afcfa841fc7b
Fred Sauer <fredsa@google.com>
2015-07-11 15:53:02 -0700

    dampen player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	float speed = .1f;
+	float speed = .15f;
 
 	void FixedUpdate () {
-		float h = Input.GetAxis ("Horizontal");
-		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal") * speed;
+		float v = Input.GetAxis ("Vertical") * speed;
 
 		float x = Mathf.Clamp (transform.localPosition.x + h, -1f, 1f);
 		float y = Mathf.Clamp (transform.localPosition.y + v, -1.5f, 1.5f);
