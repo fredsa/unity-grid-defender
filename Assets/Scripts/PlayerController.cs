ed9c5e7f8f457c55ff1656a0f4eec5f18edb3535
Fred Sauer <fredsa@google.com>
2015-07-11 15:33:39 -0700

    player movement entire grid

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	float speed = .1f;
+
+	void Start () {
+		Debug.Log (transform.localPosition);
+	}
+	
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		float x = Mathf.Clamp (transform.localPosition.x + h, -1f, 1f);
+		float y = Mathf.Clamp (transform.localPosition.y + v, -1.5f, 1.5f);
+
+		gameObject.transform.localPosition = new Vector3 (x, y, 0);
+	}
+}
