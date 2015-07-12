f364a8be1612184a90a9c5e4ddecd15c384dd011
Fred Sauer <fredsa@google.com>
2015-07-12 11:26:19 -0700

    player dynamics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,15 @@ public class PlayerController : MonoBehaviour {
 	public Transform grid;
 	public float fingerYOffset = 0f;
 
-	float speed = .4f;
+	float speed = 30f;
 	float clampX;
 	float clampy;
 	Vector3 targetPosition;
 	Plane playerPlane;
+	Rigidbody rb;
 
 	void Start() {
+		rb = GetComponent<Rigidbody> ();
 		playerPlane = new Plane(Vector3.forward, transform.position);
 		targetPosition = transform.position;
 		clampX = grid.localScale.x/2 - transform.localScale.x/2;
@@ -20,41 +22,22 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
+
+		Vector3 direction;
+
 		if (Input.GetMouseButton (0)) {
 			Vector3 pos = Input.mousePosition;
 			pos.y = pos.y + fingerYOffset;
-			Ray ray = Camera.main.ScreenPointToRay(pos);
+			Ray ray = Camera.main.ScreenPointToRay (pos);
 			float distance;
-			playerPlane.Raycast(ray, out distance);
-			targetPosition = ray.GetPoint(distance);
+			playerPlane.Raycast (ray, out distance);
+			targetPosition = ray.GetPoint (distance);
 
-//			Debug.Log (Input.mousePosition);
-//			targetPosition = Input.mousePosition;
-//			targetPosition.z = 0;
-//			targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-//			Debug.Log (targetPosition);
-//			targetPosition = Input.mousePosition;
-			transform.position = targetPosition;
-			return;
+			direction = targetPosition - transform.position;
+		} else {
+			direction = new Vector3(Input.GetAxisRaw ("Horizontal"),Input.GetAxisRaw ("Vertical"), 0);
 		}
 
-//		if (Input.touchCount > 0) {
-//			targetPosition = Input.GetTouch(0).position;
-//			transform.position = targetPosition;
-//			return;
-//		}
-
-		float x = transform.localPosition.x;
-		float y = transform.localPosition.y;
-
-		float h = Input.GetAxis ("Horizontal") * speed;
-		float v = Input.GetAxis ("Vertical") * speed;
-
-		x = Mathf.Clamp (x + h, -clampX, clampX);
-		y = Mathf.Clamp (y + v, -clampy, clampy);
-
-		float z = transform.localPosition.z;
-
-		gameObject.transform.localPosition = new Vector3 (x, y, z);
+		rb.velocity = direction.normalized * speed;
 	}
 }
