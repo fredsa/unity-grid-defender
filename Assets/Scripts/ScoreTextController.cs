8803446c332edd1528ce3775e4cddd1549de1553
Fred Sauer <fredsa@google.com>
2015-07-12 22:43:42 -0700

    empty score

---
+++ Assets/Scripts/ScoreTextController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreTextController : MonoBehaviour {
+
+	int score;
+	Text scoreText;
+	Animator animator;
+
+	void Start () {
+	    scoreText = gameObject.GetComponent<Text> ();
+	    animator = gameObject.GetComponent<Animator> ();
+		ResetScore ();
+	}
+
+	public void ResetScore() {
+		score = 0;
+		UpdateScore ();
+	}
+
+	public void AddPoints(int points) {
+		score += points;
+		animator.SetTrigger ("ValueChanged");
+		UpdateScore ();
+	}
+
+	void UpdateScore () {
+		scoreText.text = string.Format ("{0,-4:n0}", score);
+	}
+
+}
