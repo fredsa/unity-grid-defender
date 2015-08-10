1b6513274126fb115a4a15e987bcff36cdf6f70e
Fred Sauer <fredsa@google.com>
2015-08-09 20:17:09 -0700

    shield rings

---
+++ Assets/_Scripts/RandomRotatorController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotatorController : MonoBehaviour {
+
+	void Start () {
+		GetComponent<Rigidbody> ().angularVelocity = Random.rotation.eulerAngles * 5f;
+	}
+	
+}
