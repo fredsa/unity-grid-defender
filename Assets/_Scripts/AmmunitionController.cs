bbd2a2d887da70de4cf1a02e5fa746ad7180a3a8
Fred Sauer <fredsa@google.com>
2015-08-11 14:51:51 -0700

    shield has limited invinsibility

---
+++ Assets/_Scripts/AmmunitionController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmunitionController : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+	public GameObject minePrefab;
+
+	public int hitPoints = 3;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Enemy") || other.CompareTag ("EnemyObstacle")) {
+			int points = 10;
+			Color otherColor = other.gameObject.GetComponent<MeshRenderer> ().material.color;
+			otherColor.a = 1f;
+			
+			GameObject explosion = Instantiate(explosionPrefab, other.transform.position, Quaternion.identity) as GameObject;
+			explosion.GetComponentInChildren<ParticleSystem>().startColor = otherColor;
+			explosion.GetComponentInChildren<TextMesh>().color = otherColor;
+			explosion.GetComponentInChildren<TextMesh>().text = string.Format("{0}", points);
+			
+			if (other.CompareTag ("Enemy")) {
+				GameObject mine = Instantiate(minePrefab, other.transform.position, Quaternion.identity) as GameObject;
+				otherColor *= .8f;
+				mine.GetComponentInChildren<MeshRenderer>().material.color = otherColor;
+				mine.GetComponentInChildren<Light>().color = otherColor;
+			}
+			
+			Destroy(other.gameObject);
+			FindObjectOfType<GameController>().AddPoints(points);
+			if (--hitPoints <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
+}
--- Assets/_Scripts/BulletController.cs
+++ Assets/_Scripts/BulletController.cs
@@ -3,11 +3,7 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour {
 
-	public GameObject explosionPrefab;
-	public GameObject minePrefab;
-
 	private float bulletSpeed = 40f;
-	private int hitPoints = 3;
 
 	private Rigidbody rb;
 
@@ -16,29 +12,4 @@ public class BulletController : MonoBehaviour {
 		rb.velocity = transform.up.normalized * bulletSpeed;
 	}
 
-	void OnTriggerEnter(Collider other) {
-		if (other.CompareTag ("Enemy") || other.CompareTag ("EnemyObstacle")) {
-			int points = 10;
-			Color otherColor = other.gameObject.GetComponent<MeshRenderer> ().material.color;
-			otherColor.a = 1f;
-
-			GameObject explosion = Instantiate(explosionPrefab, other.transform.position, Quaternion.identity) as GameObject;
-			explosion.GetComponentInChildren<ParticleSystem>().startColor = otherColor;
-			explosion.GetComponentInChildren<TextMesh>().color = otherColor;
-			explosion.GetComponentInChildren<TextMesh>().text = string.Format("{0}", points);
-
-			if (other.CompareTag ("Enemy")) {
-				GameObject mine = Instantiate(minePrefab, other.transform.position, Quaternion.identity) as GameObject;
-				otherColor *= .8f;
-				mine.GetComponentInChildren<MeshRenderer>().material.color = otherColor;
-				mine.GetComponentInChildren<Light>().color = otherColor;
-			}
-			
-			Destroy(other.gameObject);
-			FindObjectOfType<GameController>().AddPoints(points);
-			if (--hitPoints <= 0) {
-				Destroy(gameObject);
-			}
-		}
-	}
 }
