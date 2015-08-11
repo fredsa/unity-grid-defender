5c5b42cabeb21ffaf950b31f5ccb01b5ae21d840
Fred Sauer <fredsa@google.com>
2015-08-11 14:42:41 -0700

    player shield

---
+++ Assets/_Scripts/ShieldRingController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldRingController : MonoBehaviour {
+
+	void Start () {
+		GetComponent<Rigidbody> ().angularVelocity = transform.up * 4f;
+		transform.rotation = Quaternion.LookRotation (new Vector3(1f, 0.3f, 0f));
+	}
+
+}
