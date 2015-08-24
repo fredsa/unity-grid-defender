d9752ea8010a6776c520fb983d2abb17f08799d4
Fred Sauer <fredsa@google.com>
2015-08-23 19:37:04 -0700

    scroll grid background

---
+++ Assets/_Scripts/GridTextureController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridTextureController : MonoBehaviour {
+
+	private float speed = 0.8f;
+
+	private Material material;
+	private Vector2 offset = new Vector2();
+	private float textureScaleX;
+	private float textureScaleY;
+//	private Transform playerTransform;
+
+	void Start () {
+//		playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+		material = GetComponent<MeshRenderer> ().material;
+		textureScaleX = material.mainTextureScale.x;
+		textureScaleY = material.mainTextureScale.y;
+	}
+	
+	void Update () {
+//		offset.x = playerTransform.position.x / textureScaleX;
+//		offset.y = playerTransform.position.y / textureScaleY + Time.time * speed;
+		offset.y = Time.time * speed;
+		material.mainTextureOffset = offset;
+	}
+}
