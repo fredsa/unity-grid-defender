cadede67f1abbb1a6aa45b9ff754ea3a094870b6
Fred Sauer <fredsa@google.com>
2015-08-31 18:10:39 -0700

    Android TV remote and DPAD support

---
+++ Assets/_Scripts/StartButtonController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartButtonController : MonoBehaviour {
+
+	void Update () {
+		// Android TV remote and DPAD support
+		if (Input.GetButtonUp ("Submit")) {
+			gameObject.GetComponent<Button>().onClick.Invoke();
+		}
+	}
+}
