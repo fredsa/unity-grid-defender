2e81c07e9fa3fbb733ba9d1a1a037d17d93648d0
Fred Sauer <fredsa@google.com>
2015-10-04 16:41:40 -0700

    avoid FPS NaN when time scale is briefly set to zero

--- Assets/_Scripts/FpsCounterController.cs
+++ Assets/_Scripts/FpsCounterController.cs
@@ -22,16 +22,18 @@ public class FpsCounterController : MonoBehaviour
 		
 	void Update ()
 	{
-		float interp = Time.deltaTime / (0.5f + Time.deltaTime);
-		float currentFPS = 1.0f / Time.deltaTime;
-		fps = Mathf.Lerp (fps, currentFPS, interp);
-		text.text = string.Format ("{0:0} fps", fps);
+		if (Time.deltaTime != 0) {
+			float interp = Time.deltaTime / (0.5f + Time.deltaTime);
+			float currentFPS = 1.0f / Time.deltaTime;
+			fps = Mathf.Lerp (fps, currentFPS, interp);
+			text.text = string.Format ("{0:0} fps", fps);
 			
-		// adjust red/green color
-		float fps_clamped = Mathf.Clamp (fps, BAD_FPS, GOOD_FPS);
-		float green_red_ratio = (fps_clamped - BAD_FPS) / (GOOD_FPS - BAD_FPS);
-		color.g = green_red_ratio;
-		color.r = 1f - green_red_ratio;
-		text.color = color;
+			// adjust red/green color
+			float fps_clamped = Mathf.Clamp (fps, BAD_FPS, GOOD_FPS);
+			float green_red_ratio = (fps_clamped - BAD_FPS) / (GOOD_FPS - BAD_FPS);
+			color.g = green_red_ratio;
+			color.r = 1f - green_red_ratio;
+			text.color = color;
+		}
 	}
 }
