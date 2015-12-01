4019e37b928d200f3ff89bccf5ea32dc520476d8
Fred Sauer <fredsa@google.com>
2015-12-01 10:18:37 -0800

    reinstall.sh

---
+++ reinstall.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -ue
+
+MANIFEST_PATH=Assets/Plugins/Android/AndroidManifest.xml
+
+activity=com.unity3d.player.UnityPlayerActivity
+root=$( dirname $0 )
+
+# Determine APK name
+if [ $# -ge 1 ]
+then
+  apk=$1
+else
+  # Look for APKs
+  apk=$( ls -1 "$root"/*.apk )
+  apk_count=$(( $( echo "$apk" | wc -l ) ))
+  if [ $apk_count -ne 1 ]
+  then
+    echo "ERROR: Found $apk_count APKs but expecting exactly 1" 1>&2
+    ls -l $apk 1>&2
+    exit 1
+  fi
+fi
+
+# Determine Android package name
+if [ $# -ge 2 ]
+then
+  pkg=$2
+else
+  pkg=$( basename $apk | sed -e 's/.apk$//' )
+fi
+
+# Determine overriding Android activity name from AndroidManifest.xml
+if [ -f "$MANIFEST_PATH" ]
+then
+  echo
+  echo "Extracting activity name from $MANIFEST_PATH …"
+  activity=$( grep '<activity android:name="' "$MANIFEST_PATH" | cut -d '"' -f 2 )
+fi
+
+
+# Begin actual un/re-install and launch
+echo
+echo "Uninstalling $pkg …"
+adb shell pm uninstall $pkg || true
+
+echo
+echo "Installing $apk …"
+adb install -r -g $apk
+
+echo
+echo "Launching $pkg/$activity …"
+adb shell am start -n $pkg/$activity
