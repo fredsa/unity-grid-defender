75ba67761d3ba89a44942795149482acc4903759
Fred Sauer <fredsa@google.com>
2015-12-26 13:52:33 -0800

    FPS counter uses unscaledDeltaTime

--- Assets/_Scripts/FpsCounterController.cs
+++ Assets/_Scripts/FpsCounterController.cs
@@ -24,18 +24,16 @@ public class FpsCounterController : MonoBehaviour
 		
 	void Update ()
 	{
-		if (Time.deltaTime != 0) {
-			float interp = Time.deltaTime / (0.5f + Time.deltaTime);
-			float currentFPS = 1.0f / Time.deltaTime;
-			fps = Mathf.Lerp (fps, currentFPS, interp);
-			text.text = string.Format (FPS_FORMAT, fps);
-			
-			// adjust red/green color
-			float fps_clamped = Mathf.Clamp (fps, BAD_FPS, GOOD_FPS);
-			float green_red_ratio = (fps_clamped - BAD_FPS) / (GOOD_FPS - BAD_FPS);
-			color.g = green_red_ratio;
-			color.r = 1f - green_red_ratio;
-			text.color = color;
-		}
+		float interp = Time.unscaledDeltaTime / (0.5f + Time.unscaledDeltaTime);
+		float currentFPS = 1.0f / Time.unscaledDeltaTime;
+		fps = Mathf.Lerp (fps, currentFPS, interp);
+		text.text = string.Format (FPS_FORMAT, fps);
+
+		// adjust red/green color
+		float fps_clamped = Mathf.Clamp (fps, BAD_FPS, GOOD_FPS);
+		float green_red_ratio = (fps_clamped - BAD_FPS) / (GOOD_FPS - BAD_FPS);
+		color.g = green_red_ratio;
+		color.r = 1f - green_red_ratio;
+		text.color = color;
 	}
 }
